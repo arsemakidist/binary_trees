be8dc7b36c3775e0a68960590548b0509a5ee823
arsemakidist <kidist.shiferaw@aau.edu.et>
2022-12-14 00:29:05 -0800

    my task

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,5 +1,4 @@
 #include "binary_trees.h"
-
 /**
 * binary_tree_node - creates a binary tree node
 * @parent: pointer to the parent node of the node to create
@@ -10,15 +9,13 @@
 */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-
-	binary_tree_t *new;
-
-	new = malloc(sizeof(binary_tree_t));
-	if (!new)
-        	return (NULL);
-	new->n = value;
-	new->parent = parent;
-	new->left = NULL;
-	new->right = NULL;
-	return (new);
+binary_tree_t *new;
+new = malloc(sizeof(binary_tree_t));
+if (!new)
+return (NULL);
+new->n = value;
+new->parent = parent;
+new->left = NULL;
+new->right = NULL;
+return (new);
 }
